00b8707481aba350fb8dc1d43081d54b60d920d2
thistuna <thistuna@gmail.com>
2019-03-02 21:39:26 +0900

    ディレクトリの取得を追加

--- dmerge.cpp
+++ dmerge.cpp
@@ -6,7 +6,10 @@
 namespace fsys = std::filesystem;
 
 void help(){
-	std::cout << "dmerge source to [option]\n" << std::endl;
+	std::cout << "ディレクトリの内容を別のディレクトリにコピーします．" << std::endl;
+	std::cout << "dmerge コピー元 コピー先 [オプション]\n" << std::endl;
+
+	std::cout << "　オプション" << std::endl;
 	std::cout << "-skip     : 重複ファイルをスキップ(default)" << std::endl;
 	std::cout << "-sizeup   : ファイルサイズ大優先" << std::endl;
 	std::cout << "-sizedown : ファイルサイズ小優先" << std::endl;
@@ -19,15 +22,45 @@ int Main(std::vector<std::string> args)
 		help();
 	}
 
-	fsys::path inpath = args[1];
-	if (!fsys::is_directory(inpath))
+	fsys::path indirpath = args[1];
+	if (!fsys::is_directory(indirpath)) {
+		std::cout << "コピー元ディレクトリが見つかりません." << std::endl;
+		help();
 		return 0;
-	fsys::path outpath = args[2];
-	if (!fsys::is_directory(outpath))
+	}
+	fsys::path outdirpath = args[2];
+	if (!fsys::is_directory(outdirpath)) {
+		std::cout << "コピー先ディレクトリが見つかりません." << std::endl;
+		help();
 		return 0;
+	}
 
-	for (const fsys::directory_entry& x : fsys::recursive_directory_iterator(inpath)) {
-		std::cout << x.path() << std::endl;
+	enum class SizeFlag{skip, sizeup, sizedown,err};
+	SizeFlag sizeFlag = SizeFlag::skip;
+	bool optionerr = false;
+	for (int i = 3; i < args.size(); ++i) {
+		if (args[i] == "-skip")
+			sizeFlag = SizeFlag::skip;
+		else if (args[i] == "-sizeup")
+			sizeFlag = SizeFlag::sizeup;
+		else if (args[i] == "-sizedown")
+			sizeFlag = SizeFlag::sizedown;
+		else {
+			std::cout << "unknown parameter \"" << args[i] << "\"" << std::endl;
+			optionerr = true;
+		}
+		if (optionerr) {
+			return 0;
+		}
+	}
+	std::cout << "Mode:" << (sizeFlag == SizeFlag::skip ? "Skip" : sizeFlag == SizeFlag::sizeup ? "SizeUp" : sizeFlag == SizeFlag::sizedown ? "SizeDown" : "") << std::endl;
+
+	std::cout << "outdir: " << outdirpath.string() << std::endl;
+	for (const fsys::directory_entry& x : fsys::recursive_directory_iterator(indirpath)) {
+		fsys::path relpath = x.path().lexically_relative(indirpath);
+		fsys::path outpath = outdirpath.string() + "\\" + relpath.string();
+		outpath = outpath.lexically_normal();
+		std::cout << std::setw(24) << std::left << relpath.string() << " | " << std::setw(24) << std::left << outpath.string() << " | " << (x.is_directory()?"DIR":(x.is_regular_file()?"FILE":"")) << std::endl;
 	}
 
 	return 0;
