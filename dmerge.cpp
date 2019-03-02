389e39450bafcb9cf9298954240dfeac2166ffe0
thistuna <thistuna@gmail.com>
2019-03-02 22:46:51 +0900

    ファイルサイズを見てコピーを追加

--- dmerge.cpp
+++ dmerge.cpp
@@ -16,6 +16,17 @@ void help(){
 	exit(0);
 }
 
+int GetDigit(int num) {
+	if (!num)return 1;
+	if (num < 0)num = -num;
+	int count = 0;
+	while (num) {
+		num /= 10;
+		count++;
+	}
+	return count;
+}
+
 int Main(std::vector<std::string> args)
 {
 	if(args.size() < 3){
@@ -50,17 +61,71 @@ int Main(std::vector<std::string> args)
 			optionerr = true;
 		}
 		if (optionerr) {
+			help();
 			return 0;
 		}
 	}
 	std::cout << "Mode:" << (sizeFlag == SizeFlag::skip ? "Skip" : sizeFlag == SizeFlag::sizeup ? "SizeUp" : sizeFlag == SizeFlag::sizedown ? "SizeDown" : "") << std::endl;
 
 	std::cout << "outdir: " << outdirpath.string() << std::endl;
+
+	int filecount = 0;
+	int filenum = 0;
+	for (const fsys::directory_entry& x : fsys::recursive_directory_iterator(indirpath)) {
+		filenum++;
+	}
 	for (const fsys::directory_entry& x : fsys::recursive_directory_iterator(indirpath)) {
 		fsys::path relpath = x.path().lexically_relative(indirpath);
 		fsys::path outpath = outdirpath.string() + "\\" + relpath.string();
 		outpath = outpath.lexically_normal();
-		std::cout << std::setw(24) << std::left << relpath.string() << " | " << std::setw(24) << std::left << outpath.string() << " | " << (x.is_directory()?"DIR":(x.is_regular_file()?"FILE":"")) << std::endl;
+
+		std::cout << "[" << std::setw(GetDigit(filenum)) << ++filecount << "/" << filenum << "]";
+
+		if (fsys::exists(outpath)) {
+			if (fsys::is_directory(outpath)) {
+				std::cout << "[DIR ][SKIP]";
+				std::cout << x.path().string() << " > " << outpath.string() << std::endl;
+			}
+			else if (fsys::is_regular_file(outpath)) {
+				std::cout << "[FILE]";
+				if (sizeFlag == SizeFlag::skip) {
+					std::cout << "[SKIP]";
+					std::cout << x.path().string() << " > " << outpath.string() << std::endl;
+				}
+				else if (sizeFlag == SizeFlag::sizedown) {
+					if (x.file_size() >= fsys::file_size(outpath)) {
+						std::cout << "[SKIP]";
+						std::cout << x.path().string() << " > " << outpath.string() << std::endl;
+					}
+					else{
+						std::cout << x.path().string() << " > " << outpath.string() << std::endl;
+						fsys::copy_file(x,outpath,fsys::copy_options::overwrite_existing);
+					}
+				}
+				else if (sizeFlag == SizeFlag::sizeup) {
+					if (x.file_size() <= fsys::file_size(outpath)) {
+						std::cout << "[SKIP]";
+						std::cout << x.path().string() << " > " << outpath.string() << std::endl;
+					}
+					else {
+						std::cout << x.path().string() << " > " << outpath.string() << std::endl;
+						fsys::copy_file(x, outpath, fsys::copy_options::overwrite_existing);
+					}
+				}
+			}
+		}
+		else {
+			if (x.is_directory()) {
+				std::cout << "[DIR ]";
+				std::cout << x.path().string() << " > " << outpath.string() << std::endl;
+				fsys::create_directory(outpath);
+			}
+			else if (x.is_regular_file()){
+				std::cout << "[FILE]";
+				std::cout << x.path().string() << " > " << outpath.string() << std::endl;
+				fsys::copy_file(x, outpath);
+			}
+		}
 	}
 
 	return 0;
